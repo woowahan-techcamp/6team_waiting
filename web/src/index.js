80522fcb5814100b45a2e764d2463bc15d66438e
haeun.kim <haeungun@gmail.com>
2017-08-02 21:29:05 +0900

    [이슈#5] Firerbase 세팅

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,20 @@
+import * as firebase from "firebase";
+
 import _ from "./util.js";
 import "./index.css";
 
 const root = document.querySelector("#root");
 root.innerHTML = `<p>Hello Waiting!!</p>`;
 _.log(root.innerHTML);
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyCOh94raCe9z-r0QcOBCCU2CJmIKFcGdyk",
+    authDomain: "baeminwaiting.firebaseapp.com",
+    databaseURL: "https://baeminwaiting.firebaseio.com",
+    projectId: "baeminwaiting",
+    storageBucket: "baeminwaiting.appspot.com",
+    messagingSenderId: "352546507957"
+};
+firebase.initializeApp(config);
+console.log(firebase);
