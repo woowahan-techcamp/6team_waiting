09421403d61c813bcf234673c2f8f588d9011a94
haeun.kim <haeungun@gmail.com>
2017-08-03 21:34:20 +0900

    [이슈#7] Auth service 구현

--- web/src/index.js
+++ web/src/index.js
@@ -1,20 +1,12 @@
-import * as firebase from "firebase";
-
 import _ from "./util.js";
-import "./index.css";
+import "./style.css";
+
+import service from "./services/service.js";
+
 
 const root = document.querySelector("#root");
 root.innerHTML = `<p>Hello Waiting!!</p>`;
 _.log(root.innerHTML);
 
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyCOh94raCe9z-r0QcOBCCU2CJmIKFcGdyk",
-    authDomain: "baeminwaiting.firebaseapp.com",
-    databaseURL: "https://baeminwaiting.firebaseio.com",
-    projectId: "baeminwaiting",
-    storageBucket: "baeminwaiting.appspot.com",
-    messagingSenderId: "352546507957"
-};
-firebase.initializeApp(config);
-console.log(firebase);
+// Sign up test 
+service.signUpUser("test@test.com", "123456", null);
