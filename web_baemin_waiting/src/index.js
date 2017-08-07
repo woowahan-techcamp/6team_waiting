19c7d508111b4f72ab747358fa077e08d72daed3
haeun.kim <haeungun@gmail.com>
2017-08-07 15:14:06 +0900

    [이슈#7] 레스토랑 등록 외 기능 구현

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -8,5 +8,9 @@ const root = document.querySelector("#root");
 root.innerHTML = `<p>Hello Waiting!!</p>`;
 _.log(root.innerHTML);
 
-// Sign up test 
-service.signUpUser("test333@test.com", "123456", "test", "owner", "010-1111-2222", null);
+// Register test
+// (owner, title, store_tel, address, picture, description, is_opened)
+service.registerRestaurant("lnTusrM4OIW7GOfhFiawb7GNSWC3", "Restaurant House", "02-1111-2222", "서울시 송파구", null, "This is test store", false);
+
+service.getUserDataByUid("lnTusrM4OIW7GOfhFiawb7GNSWC3")
+    .then((v) => console.log(v));
