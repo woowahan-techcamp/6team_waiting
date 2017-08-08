46f1ce0b3a5b9652d837fbdc70412c93c72e9842
haeun.kim <haeungun@gmail.com>
2017-08-08 16:07:33 +0900

    [이슈#29] 메인 메뉴 템플릿 구현

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -4,10 +4,26 @@ import "./style.css";
 import service from "./js/services/service.js";
 
 
-const root = document.querySelector("#root");
-const html = Handlebars.templates["home"];
-root.innerHTML = html({"name": "home"});
+const menu = [{
+                "id": "home",
+                "content": "홈"
+            },{
+                "id": "introduce",
+                "content": "서비스소개"
+            },{
+                "id": "manage",
+                "content": "가게관리"
+            },{
+                "id": "auth",
+                "content": "로그인"
+            }];
 
+const root = document.querySelector("#root");
+const html = Handlebars.templates["main"];
+root.innerHTML = html(menu);
+
+const view = document.querySelector("#view");
+view.innerHTML = "Hello!";
 // Get user data test
 service.getUserDataByUid("lnTusrM4OIW7GOfhFiawb7GNSWC3")
     .then((v) => console.log(v));
--- web_baemin_waiting/src/js/templates/templates.js
+++ web_baemin_waiting/src/js/templates/templates.js
@@ -1,10 +1,18 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['home'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper;
+templates['main'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "<section id=\"home\">\n  This is "
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"name","hash":{},"data":data}) : helper)))
-    + ".\n</section>";
+  return "      <li id="
+    + alias2(alias1((depth0 != null ? depth0.id : depth0), depth0))
+    + ">"
+    + alias2(alias1((depth0 != null ? depth0.content : depth0), depth0))
+    + "</li>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<section id=\"home\">\n  <div>\n    <ul>\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    </ul>\n  </div>\n  <div id=\"view\"></div>\n</section>";
 },"useData":true});
 })();
