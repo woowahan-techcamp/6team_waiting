e644b3071e5bcedb88dc4a4ce47a1ec9f06de175
haeun.kim <haeungun@gmail.com>
2017-08-09 16:10:32 +0900

    [이슈#29] 메인 페이지 디자인

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -1,4 +1,5 @@
 import _ from "./util.js";
+
 import "./style.css";
 import "./style/main.css";
 
@@ -6,13 +7,13 @@ import service from "./js/services/service.js";
 
 
 const menu = [{
-                "id": "home",
+                "id": "navi-home",
                 "content": "홈"
             },{
-                "id": "introduce",
+                "id": "navi-introduce",
                 "content": "서비스소개"
             },{
-                "id": "manage",
+                "id": "navi-manage",
                 "content": "가게관리"
             },{
                 "id": "auth",
@@ -21,6 +22,13 @@ const menu = [{
 
 const header = document.querySelector("#header");
 const headerHtml = Handlebars.templates["header"];
-console.log(header);
 header.innerHTML = headerHtml(menu);
+
+const main = document.querySelector("#main");
+const mainHtml = Handlebars.templates["main"];
+main.innerHTML = mainHtml();
+
+const footer = document.querySelector("#footer");
+const footerHtml = Handlebars.templates["footer"];
+footer.innerHTML = footerHtml();
 
