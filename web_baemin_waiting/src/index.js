b3ede81949862efc3aaade01f5cdf71bd1911dcc
haeun.kim <haeungun@gmail.com>
2017-08-10 18:18:18 +0900

    index.html 수정

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -1,9 +1,6 @@
 import _ from "./util.js";
 
 import "./style.css";
-import "./style/header.css";
-import "./style/main.css";
-import "./style/footer.css";
 
 import service from "./js/services/service.js";
 
