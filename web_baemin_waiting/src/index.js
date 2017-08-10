5d24c87d3c50a63241207e6fb5de7bb04a0958ff
haeun.kim <haeungun@gmail.com>
2017-08-10 18:58:37 +0900

    index.js 정리

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -19,15 +19,8 @@ const menu = [{
                 "content": "로그인"
             }];
 
-const header = document.querySelector("#header");
-const headerHtml = Handlebars.templates["header"];
-header.innerHTML = headerHtml(menu);
+const root = document.querySelector("#root");
+const homeHtml = Handlebars.templates["home"];
+root.innerHTML = homeHtml();
 
-const main = document.querySelector("#main");
-const mainHtml = Handlebars.templates["main"];
-main.innerHTML = mainHtml();
-
-const footer = document.querySelector("#footer");
-const footerHtml = Handlebars.templates["footer"];
-footer.innerHTML = footerHtml();
 
