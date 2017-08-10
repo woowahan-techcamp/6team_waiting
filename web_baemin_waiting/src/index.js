4f5abc52254df02cd1ef8c89d176abd8bacf7770
haeun.kim <haeungun@gmail.com>
2017-08-10 18:48:29 +0900

    [이슈#1] 웹 public 파일 생성

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -4,9 +4,30 @@ import "./style.css";
 import service from "./js/services/service.js";
 
 
+<<<<<<< Updated upstream
 const root = document.querySelector("#root");
 const html = Handlebars.templates["home"];
 root.innerHTML = html({"name": "home"});
+=======
+const menu = [{
+                "id": "navi-home",
+                "content": "홈"
+            },{
+                "id": "navi-introduce",
+                "content": "서비스소개"
+            },{
+                "id": "navi-manage",
+                "content": "가게관리"
+            },{
+                "id": "auth",
+                "content": "로그인"
+            }];
+
+const root = document.querySelector("#root");
+// const headerHtml = Handlebars.templates["header"];
+// header.innerHTML = headerHtml(menu);
+
+>>>>>>> Stashed changes
 
 // Get user data test
 service.getUserDataByUid("lnTusrM4OIW7GOfhFiawb7GNSWC3")
