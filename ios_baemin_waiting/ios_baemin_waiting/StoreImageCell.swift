98ada92a37eeb1c6f7c760006fb7fd031ba7c284
Hong Seong Ho <buk4130@gmail.com>
2017-08-11 10:35:05 +0900

    [이슈 #43] DetailView 설정

---
+++ ios_baemin_waiting/ios_baemin_waiting/StoreImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  StoreImageCell.swift
+//  ios_baemin_waiting
+//
+//  Created by woowabrothers on 2017. 8. 11..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class StoreImageCell: UITableViewCell {
+
+    @IBOutlet weak var storeImage: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
