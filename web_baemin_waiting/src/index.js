6bdde6ec78ccec3be0b013e62e66abb192406be9
haeun.kim <haeungun@gmail.com>
2017-08-11 11:20:48 +0900

    util 에 setTemplateInHtml 기능 구현

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -6,7 +6,7 @@ import "./style/home.css";
 import service from "./js/services/service.js";
 
 
-const desc = {
+const homeData = {
                 "main-desc" : ["맛집의 손님들을 위한 대기 서비스", "이제 배민웨이팅으로 고객들에게 더 좋은 서비스로"],
                 "serv-desc": ["1번", "2번", "3번"],
                 "navi-list": [
@@ -16,8 +16,7 @@ const desc = {
                 ]
             };
 
-const root = document.querySelector("#root");
-const homeHtml = Handlebars.templates["home"];
-root.innerHTML = homeHtml(desc);
+_.setTemplateInHtml("#root", "home", homeData);
+
 
 
--- web_baemin_waiting/src/util.js
+++ web_baemin_waiting/src/util.js
@@ -9,6 +9,12 @@ const _ = {
         oReq.addEventListener("load", callback);
         oReq.open("GET", url);
         oReq.send();
+    },
+
+    setTemplateInHtml(position, temp, data) {
+        const pos = document.querySelector(position);
+        const html = Handlebars.templates[temp];
+        pos.innerHTML = html(data);
     }
 }
 
