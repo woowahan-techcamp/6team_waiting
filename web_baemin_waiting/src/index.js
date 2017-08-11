52005ede82c1944a10b40381c87ad9074bc3016c
haeun.kim <haeungun@gmail.com>
2017-08-11 10:18:18 +0900

    홈 템플릿에서 description 분리

--- web_baemin_waiting/src/index.js
+++ web_baemin_waiting/src/index.js
@@ -6,22 +6,18 @@ import "./style/home.css";
 import service from "./js/services/service.js";
 
 
-const menu = [{
-                "id": "navi-home",
-                "content": "홈"
-            },{
-                "id": "navi-introduce",
-                "content": "서비스소개"
-            },{
-                "id": "navi-manage",
-                "content": "가게관리"
-            },{
-                "id": "auth",
-                "content": "로그인"
-            }];
+const desc = {
+                "main-desc" : ["맛집의 손님들을 위한 대기 서비스", "이제 배민웨이팅으로 고객들에게 더 좋은 서비스로"],
+                "serv-desc": ["1번", "2번", "3번"],
+                "navi-list": [
+                            {"id": "navi-home", "to":"홈"},
+                            {"id": "navi-mypage", "to":"마이페이지"},
+                            {"id": "navi-manage", "to":"가게 관리"}
+                ]
+            };
 
 const root = document.querySelector("#root");
 const homeHtml = Handlebars.templates["home"];
-root.innerHTML = homeHtml();
+root.innerHTML = homeHtml(desc);
 
 
--- web_baemin_waiting/src/js/templates/templates.js
+++ web_baemin_waiting/src/js/templates/templates.js
@@ -1,6 +1,26 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['home'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"view\"></div>\n\n<div class=\"home\">\n    <h1>배민 웨이팅</h1>\n    <p>맛집의 손님들을 위한 대기 서비스</br>이제 배민웨이팅으로 고객들에게 더 좋은 서비스로</p>\n    <div class=\"btn-area\">\n        <button id=\"btn-intro\">서비스 소개</button>\n        <button id=\"btn-go-login\">로그인 하기</button>\n    </div>\n</div>\n\n\n<div class=\"intro\">\n    <span id=\"btn-intro-close\" class=\"btn-close\"><img src=\"./close.png\"></span>\n</div>\n\n\n<div class=\"sign-in\">\n    <span id=\"btn-login-close\" class=\"btn-close\"><img src=\"./close.png\"></span>\n    <div class=\"sign-area\">\n\n        <input type=\"text\">\n        <input type=\"password\">\n        <button id=\"btn-login\">로그인</button>\n        <button id=\"btn-go-sign-up\">회원가입 하기</button>\n    </div>\n</div>\n\n\n<div class=\"sign-up\">\n    <span id=\"btn-sign-close\" class=\"btn-close\"><img src=\"./close.png\"></span>\n    <span class=\"sign-up-title\">WELCOME</span>\n    <div class=\"sign-up-form\">\n        <dl>\n            <dt>이름</dt>\n            <dd><input type=\"text\"></dd>\n            <dt>아이디</dt>\n            <dd><input type=\"text\"></dd>\n            <dt>비밀번호</dt>\n            <dd><input type=\"password\"></dd>\n            <dt>전화번호</dt>\n            <dd><input type=\"text\"></dd>\n        </dl>\n    </div>\n    <button id=\"btn-sign-up\">회원가입</button>\n</div>\n\n<div class=\"nav\">\n    <ul>\n        <li id=\"navi-home\">홈</li>\n        <li>마이 페이지</li>\n        <li>가게 관리</li>\n    </ul>\n</div>\n\n<div class=\"board\">\n    <div class=\"not-register\">\n        <span>등록된 가게가 없습니다.</span>\n        <span>먼저 가게를 등록해주세요.</span>\n        <button id=\"btn-go-register\">가게 등록 하기</button>\n    </div>\n</div>\n";
+templates['home'] = template({"1":function(container,depth0,helpers,partials,data) {
+    return "        <p>"
+    + container.escapeExpression(container.lambda(depth0, depth0))
+    + "</p>\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    var alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "        <li id="
+    + alias2(alias1((depth0 != null ? depth0.id : depth0), depth0))
+    + ">"
+    + alias2(alias1((depth0 != null ? depth0.to : depth0), depth0))
+    + "</li>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {});
+
+  return "<div class=\"view\"></div>\n\n\n<div class=\"home\">\n    <h1>배민 웨이팅</h1>\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["main-desc"] : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    <div class=\"btn-area\">\n        <button id=\"btn-intro\">서비스 소개</button>\n        <button id=\"btn-go-login\">로그인 하기</button>\n    </div>\n</div>\n\n\n<div class=\"intro\">\n    <!-- @TODO : haeun.kim 이미지 파일 경로 설정 -->\n    <span id=\"btn-intro-close\" class=\"btn-close\"><img src=\"./close.png\"></span>\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["serv-desc"] : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "</div>\n\n\n<div class=\"sign-in\">\n    <!-- @TODO : haeun.kim 이미지 파일 경로 설정 -->\n    <span id=\"btn-login-close\" class=\"btn-close\"><img src=\"./close.png\"></span>\n    <div class=\"sign-area\">\n\n        <input type=\"text\">\n        <input type=\"password\">\n        <button id=\"btn-login\">로그인</button>\n        <button id=\"btn-go-sign-up\">회원가입 하기</button>\n    </div>\n</div>\n\n\n<div class=\"sign-up\">\n    <!-- @TODO : haeun.kim 이미지 파일 경로 설정 -->\n    <span id=\"btn-sign-close\" class=\"btn-close\"><img src=\"./close.png\"></span>\n    <span class=\"sign-up-title\">WELCOME</span>\n    <div class=\"sign-up-form\">\n        <dl>\n            <dt>이름</dt>\n            <dd><input type=\"text\"></dd>\n            <dt>아이디</dt>\n            <dd><input type=\"text\"></dd>\n            <dt>비밀번호</dt>\n            <dd><input type=\"password\"></dd>\n            <dt>전화번호</dt>\n            <dd><input type=\"text\"></dd>\n        </dl>\n    </div>\n    <button id=\"btn-sign-up\">회원가입</button>\n</div>\n\n\n<div class=\"nav\">\n    <ul>\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["navi-list"] : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    </ul>\n</div>\n\n\n<div class=\"board\">\n    <div class=\"not-register\">\n        <span>등록된 가게가 없습니다.</span>\n        <span>먼저 가게를 등록해주세요.</span>\n        <button id=\"btn-go-register\">가게 등록 하기</button>\n    </div>\n</div>\n";
 },"useData":true});
 })();
