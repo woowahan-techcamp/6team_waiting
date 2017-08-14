e8f594ca5d117d09aed33c4e449c3b8f6585676f
haeun.kim <haeungun@gmail.com>
2017-08-14 19:31:20 +0900

    내 가게 등록하기 html

--- web_baemin_waiting/src/js/services/service.js
+++ web_baemin_waiting/src/js/services/service.js
@@ -130,7 +130,7 @@ const service = (() => {
         // @TODO : haeun.kim
         // 현재 로그인 된 계정에 등록된 가게가 있는지 없는지 확인
         hasStore() {
-            return true;
+            return false;
         }
     }
 
