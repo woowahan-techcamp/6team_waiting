aea8f39f8292427d605e80128217a3498d4fa867
woowabrothers <woowabrothers@woowabrothersui-MacBook-Pro.local>
2017-08-18 13:51:17 +0900

    [#이슈107] 웹소개페이지 기본 레이아웃 구현

--- web_baemin_waiting/src/js/templates/templates.js
+++ web_baemin_waiting/src/js/templates/templates.js
@@ -7,6 +7,14 @@ templates['home'] = template({"1":function(container,depth0,helpers,partials,dat
 },"3":function(container,depth0,helpers,partials,data) {
     var alias1=container.lambda, alias2=container.escapeExpression;
 
+  return "        <div>\n            <div class=\"circle\"><img src="
+    + alias2(alias1((depth0 != null ? depth0.img : depth0), depth0))
+    + " alt=\"desc-pic\"></div>\n            <p>"
+    + alias2(alias1((depth0 != null ? depth0.content : depth0), depth0))
+    + "</p>\n        </div>\n";
+},"5":function(container,depth0,helpers,partials,data) {
+    var alias1=container.lambda, alias2=container.escapeExpression;
+
   return "        <li id="
     + alias2(alias1((depth0 != null ? depth0.id : depth0), depth0))
     + " data-dest="
@@ -14,7 +22,7 @@ templates['home'] = template({"1":function(container,depth0,helpers,partials,dat
     + ">"
     + alias2(alias1((depth0 != null ? depth0.to : depth0), depth0))
     + "</li>\n";
-},"5":function(container,depth0,helpers,partials,data) {
+},"7":function(container,depth0,helpers,partials,data) {
     var alias1=container.lambda, alias2=container.escapeExpression;
 
   return "            <li id="
@@ -29,12 +37,12 @@ templates['home'] = template({"1":function(container,depth0,helpers,partials,dat
 
   return "<div class=\"view\"></div>\n\n\n<div class=\"home\">\n    <h1>배민 웨이팅</h1>\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["main-desc"] : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    <div class=\"btn-area\">\n        <button id=\"btn-intro\">서비스 소개</button>\n        <button id=\"btn-go-store\">가게 관리 하기</button>\n    </div>\n</div>\n\n\n<div class=\"intro\">\n    <span id=\"btn-intro-close\" class=\"btn-close\"><img src=\"/dist/public/images/close.png\"></span>\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["serv-desc"] : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</div>\n\n\n<div class=\"sign-in\">\n    <span id=\"btn-login-close\" class=\"btn-close\"><img src=\"/dist/public/images/close.png\"></span>\n    <div class=\"sign-area\">\n        <img class=\"logo\" src=\"/dist/public/images/facebook-logo.png\" alt=\"logo\">\n        <input type=\"text\" id=\"login-id\">\n        <input type=\"password\" id=\"login-pwd\">\n        <p class=\"sign-warning\">아이디와 비밀번호를 확인해주세요<p>\n        <button id=\"btn-login\">로그인</button>\n        <button id=\"btn-go-sign-up\">회원가입 하기</button>\n    </div>\n</div>\n\n\n<div class=\"sign-up\">\n    <span id=\"btn-sign-close\" class=\"btn-close\"><img src=\"/dist/public/images/close.png\"></span>\n    <span class=\"sign-up-title\">WELCOME</span>\n    <div class=\"sign-up-form\">\n        <dl>\n            <dt>이름</dt>\n            <dd><input type=\"text\" id=\"sign-name\"></dd>\n            <dt>아이디</dt>\n            <dd><input type=\"text\" id=\"sign-id\"></dd>\n            <dt>비밀번호</dt>\n            <dd><input type=\"password\" id=\"sign-pwd\"></dd>\n            <dt>전화번호</dt>\n            <dd><input type=\"text\" id=\"sign-tel\"></dd>\n        </dl>\n        <button id=\"btn-sign-up\">회원가입</button>\n    </div>\n</div>\n\n\n<div class=\"nav\">\n    <ul class=\"navigator\">\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["navi-list"] : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </ul>\n    <div class=\"dropdown\">\n        <a href=\"javascript:;\"><img src=\"/dist/public/images/menu.png\" id=\"drop\"></a>\n        <ul class=\"dropdown-list\">\n"
+    + "    <div class=\"btn-area\">\n        <button id=\"btn-intro\">서비스 소개</button>\n        <button id=\"btn-go-store\">가게 관리 하기</button>\n    </div>\n</div>\n\n\n<div class=\"intro\">\n    <span id=\"btn-intro-close\" class=\"btn-close\"><img src=\"/dist/public/images/close.png\"></span>\n    <div class=\"intro-title\">배민웨이팅, 이렇게 사용하세요</div>\n    <div class=\"intro-desc\">\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["serv-desc"] : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    </div> \n</div>\n\n\n<div class=\"sign-in\">\n    <span id=\"btn-login-close\" class=\"btn-close\"><img src=\"/dist/public/images/close.png\"></span>\n    <div class=\"sign-area\">\n        <img class=\"logo\" src=\"/dist/public/images/facebook-logo.png\" alt=\"logo\">\n        <input type=\"text\" id=\"login-id\">\n        <input type=\"password\" id=\"login-pwd\">\n        <p class=\"sign-warning\">아이디와 비밀번호를 확인해주세요<p>\n        <button id=\"btn-login\">로그인</button>\n        <button id=\"btn-go-sign-up\">회원가입 하기</button>\n    </div>\n</div>\n\n\n<div class=\"sign-up\">\n    <span id=\"btn-sign-close\" class=\"btn-close\"><img src=\"/dist/public/images/close.png\"></span>\n    <span class=\"sign-up-title\">WELCOME</span>\n    <div class=\"sign-up-form\">\n        <dl>\n            <dt>이름</dt>\n            <dd><input type=\"text\" id=\"sign-name\"></dd>\n            <dt>아이디</dt>\n            <dd><input type=\"text\" id=\"sign-id\"></dd>\n            <dt>비밀번호</dt>\n            <dd><input type=\"password\" id=\"sign-pwd\"></dd>\n            <dt>전화번호</dt>\n            <dd><input type=\"text\" id=\"sign-tel\"></dd>\n        </dl>\n        <button id=\"btn-sign-up\">회원가입</button>\n    </div>\n</div>\n\n\n<div class=\"nav\">\n    <ul class=\"navigator\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["navi-list"] : depth0),{"name":"each","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    </ul>\n    <div class=\"dropdown\">\n        <a href=\"javascript:;\"><img src=\"/dist/public/images/menu.png\" id=\"drop\"></a>\n        <ul class=\"dropdown-list\">\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0["navi-list"] : depth0),{"name":"each","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "        </ul>\n    </div>\n</div>\n\n\n<div class=\"board\"></div>\n";
 },"useData":true});
 templates['input-menu'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
